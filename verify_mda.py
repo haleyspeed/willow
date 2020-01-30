d712e041475ace78e726d5abe79f2359199e80ed
Haley Speed <haley.e.speed@gmail.com>
2020-01-30 13:38:22 -0800

    Improved plot function

--- verify_mda.py
+++ verify_mda.py
@@ -17,8 +17,17 @@ print ('columns: ' + str(columns))
 print ('size: ' + str(d.size))
 
 # Plot the first 500 datapoints from ch0
-fig, ax = plt.subplots(figsize=(3, 4), frameon = False, dpi = 100)
+channel = 500 # chip number/sample_id
+x_start = -100
+x_end = 20000
+
+fig, ax = plt.subplots(figsize=(8, 8), frameon = False, dpi = 100)
 x = list(range(rows-1))
-ax.plot(x, d[0:(rows-1),0])
+y = (d[0:(rows-1), channel]) * 0.195 # scaled to microvolts 
+ax.plot(x, y, lw = 1)
+ax.set_xlabel ("unscaled datapoints")
+ax.set_ylabel("microvolts")
+ax.set_xlim(x_start,x_end)
 plt.show()
+
 
