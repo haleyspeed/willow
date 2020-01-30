bda870b1442fb66fd767cf9ae3b1da450e153f2c
Haley Speed <haley.e.speed@gmail.com>
2020-01-30 13:59:16 -0800

    Improved plot function

--- hdf5_to_mda.py
+++ hdf5_to_mda.py
@@ -1,9 +1,10 @@
-# Python script to convert Leaf Labs HDF5 output to MountainSort MDA file
-# Queries primary data for 
-# 1. sample_index 
-# 2. aux_data
-# 3. channel_data (contains the actual data we need to xfer)
-# 4. chip_live
+"""Python script to convert Leaf Labs HDF5 output to MountainSort MDA file
+Queries primary data for 
+1. sample_index 
+2. aux_data
+3. channel_data (contains the actual data we need to xfer)
+4. chip_live
+"""
 
 import h5py
 import numpy as np
@@ -14,7 +15,7 @@ import matplotlib.pyplot as plt
 
 # File Functions
 fin = '2020_01_08.h5'
-din = r'C:\Users\haley\Dropbox\Projects\LeafLabs\ms_test'
+din = r'D:\Dropbox\Projects\LeafLabs\ms_test'
 dout = din
 os.chdir (din)
 f = h5py.File(fin, 'r')
@@ -38,14 +39,14 @@ cols = out.shape[1]
 # 32bit save function 
 mdaio.writemda32(out,'raw.mda')
 
-# Plot the first 20000 datapoints from channel 500
+# (Optional) Plot the first 20000 datapoints from channel 500
 channel = 500 # chip number/sample_id
 x_start = -100
 x_end = 20000
 
 fig, ax = plt.subplots(figsize=(8, 8), frameon = False, dpi = 100)
 x = list(range(rows-1))
-y = (d[0:(rows-1), channel]) * 0.195 # scaled to microvolts 
+y = (out[0:(rows-1), channel]) * 0.195 # scaled to microvolts 
 ax.plot(x, y, lw = 1)
 ax.set_xlabel ("unscaled datapoints")
 ax.set_ylabel("microvolts")
